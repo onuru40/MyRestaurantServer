01ba46a3488079717c9a67b3310fa8fa382fe02c
onuru40 <onur.unlu40@gmail.com>
2025-09-23 23:28:03 +0300

    Products

--- MyRestaurant.API/DTOs/ProductDto/CreateProductDto.cs
+++ MyRestaurant.API/DTOs/ProductDto/CreateProductDto.cs
@@ -2,5 +2,10 @@
 {
     public class CreateProductDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
     }
 }
--- MyRestaurant.API/DTOs/ProductDto/UpdateProductDto.cs
+++ MyRestaurant.API/DTOs/ProductDto/UpdateProductDto.cs
@@ -2,5 +2,11 @@
 {
     public class UpdateProductDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
     }
 }
